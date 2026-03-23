0dce398f2f69b8bef93297056cf629ff2b36e7a9
Juneid Shaikh <90983910+juneid18@users.noreply.github.com>
2026-03-24 00:00:07 +0530

    fix(host): corrected case-sensitive import path for Header component for linux compatibility

--- host/pages/profile/index.jsx
+++ host/pages/profile/index.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import SideBar from '@/components/Dashboard/SideBar';
 import ProfileContent from '@/components/Profile/ProfileContent';
-import Header from '@/components/Dashboard/header';
+import Header from '@/components/Dashboard/Header';
 
 const ProfilePage = () => {
   return (
--- host/pages/saved/index.jsx
+++ host/pages/saved/index.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import SideBar from '@/components/Dashboard/SideBar';
 import SavedContent from '@/components/Saved/SavedContent';
-import Header from '@/components/Dashboard/header';
+import Header from '@/components/Dashboard/Header';
 
 const SavedPage = () => {
   return (
